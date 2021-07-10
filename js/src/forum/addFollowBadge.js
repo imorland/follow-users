21958e53f72385d66b419644c32df11eafc2b7eb
David Wheatley <hi@davwheat.dev>
2021-07-10 13:21:19 +0100

    Simplify badge adding

--- js/src/forum/addFollowBadge.js
+++ js/src/forum/addFollowBadge.js
@@ -5,34 +5,28 @@ import Badge from 'flarum/common/components/Badge';
 
 export default function addSubscriptionBadge() {
     extend(Discussion.prototype, 'badges', function (badges) {
-        let badge;
-
-        if (this.user() && this.user().followed()) {
-            badge = Badge.component({
-                label: app.translator.trans('ianm-follow-users.forum.badge.label'),
-                icon: 'fas fa-user-friends',
-                type: 'friend',
-            });
-        }
-
-        if (badge) {
-            badges.add('user-following', badge);
+        if (this.user?.()?.followed?.()) {
+            badges.add(
+                'user-following',
+                Badge.component({
+                    label: app.translator.trans('ianm-follow-users.forum.badge.label'),
+                    icon: 'fas fa-user-friends',
+                    type: 'friend',
+                })
+            );
         }
     });
 
     extend(User.prototype, 'badges', function (badges) {
-        let badge;
-
-        if (this.followed()) {
-            badge = Badge.component({
-                label: app.translator.trans('ianm-follow-users.forum.badge.label'),
-                icon: 'fas fa-user-friends',
-                type: 'friend',
-            });
-        }
-
-        if (badge) {
-            badges.add('user-following', badge);
+        if (this?.followed?.()) {
+            badges.add(
+                'user-following',
+                Badge.component({
+                    label: app.translator.trans('ianm-follow-users.forum.badge.label'),
+                    icon: 'fas fa-user-friends',
+                    type: 'friend',
+                })
+            );
         }
     });
 }
