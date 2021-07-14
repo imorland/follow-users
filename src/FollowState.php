8410307551ec61d8d832229be130fdd98173c313
Ian Morland <ian@morland.me>
2021-07-14 22:17:48 +0100

    Drop composite primary keys

--- src/FollowState.php
+++ src/FollowState.php
@@ -28,16 +28,6 @@ class FollowState extends AbstractModel
     protected $fillable = ['user_id', 'followed_user_id', 'subscription'];
 
     /**
-     * {@inheritDoc}
-     */
-    protected $primaryKey = ['user_id', 'followed_user_id'];
-
-    /**
-     * {@inheritDoc}
-     */
-    public $incrementing = false;
-
-    /**
      * Get the follow user subscription state for the given User.
      *
      * @param User $actor
@@ -50,20 +40,5 @@ class FollowState extends AbstractModel
         $sub = self::where('user_id', $actor->id)->where('followed_user_id', $user->id)->first();
 
         return $sub ? $sub->subscription : null;
-    }
-
-    /**
-     * Set the keys for a save update query.
-     *
-     * @param Builder $query
-     *
-     * @return Builder
-     */
-    protected function setKeysForSaveQuery($query)
-    {
-        $query->where('user_id', $this->user_id)
-              ->where('followed_user_id', $this->followed_user_id);
-
-        return $query;
     }
 }
